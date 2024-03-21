d2aff407c03ea6d0b8f1d6124e625078e3885c95
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-03-21 09:30:21 +0530

    completed the hero section, footer section  and contact section using react + tailwind css of the yummyfood site

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./Footer";
+import Contact from "./Contact";
+import HeroReact from "./HerorReact";
+export default function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <HeroReact />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
