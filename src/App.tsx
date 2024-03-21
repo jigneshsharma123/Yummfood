d8daeb175b159d21ecc2dfd2c8ad6d3366c6396e
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-03-21 11:46:51 +0530

    added the GetStarted section and modified the view of the contact section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Footer from "./Footer";
 import Contact from "./Contact";
 import HeroReact from "./HerorReact";
+import Explorer from "./Explorer";
 export default function App() {
   return (
     <>
-      {/* <Home /> */}
       <HeroReact />
+      <Explorer />
       <Contact />
       <Footer />
     </>
